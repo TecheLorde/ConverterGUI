5128b768e7eb354b33c2e865db7df65282766102
James <jamesp@protonmail.com>
2022-03-04 14:30:10 +0000

    Updated main to interact with different versions of Windows os.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from guichoice import GuiChoice
-
+from tkinter import messagebox
 gui_c = GuiChoice()
 gui_c.identify_os()
 try:
     if gui_c.operating_system == "Windows":
         print("Its Bill Gates!")
-        gui_c.call_windows_gui()
+        version = gui_c.get_os_version()
+        if version == '10' or version == '8.1' or version == '8' or version == '7':
+            gui_c.call_windows_gui()
+        else:
+            messagebox.showerror(title="Incorrect Version", message="The application works on windows 7, 8, 8.1 and 10")
 except RuntimeError:
     print("Something went wrong, contact support")
