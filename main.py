deec989293ed05263e2821ffe811db2d1358827f
James <jamesp@protonmail.com>
2022-03-04 14:51:26 +0000

    Can now display application on both Windows and Linux OS's.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ gui_c = GuiChoice()
 gui_c.identify_os()
 try:
     if gui_c.operating_system == "Windows":
-        print("Its Bill Gates!")
         version = gui_c.get_os_version()
         if version == '10' or version == '8.1' or version == '8' or version == '7':
             gui_c.call_windows_gui()
         else:
             messagebox.showerror(title="Incorrect Version", message="The application works on windows 7, 8, 8.1 and 10")
+    elif gui_c.operating_system == "Linux":
+        gui_c.call_linux_gui()
+
 except RuntimeError:
     print("Something went wrong, contact support")
